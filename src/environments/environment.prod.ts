0922cff0f6b117be416ec4d42e2091b55ac94408
Jefferson Pinchao <jeffersonpinchao@hotmail.com>
2023-11-27 15:27:09 -0500

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@
 
 export const environment = {
     production: true,
-    //base_url: 'http://localhost:4000/api/v1'
-    base_url: 'https://inveservice-fgl-41fc0eb645b0.herokuapp.com/api/v1'
+    base_url: 'http://localhost:4000/api/v1'
+    //base_url: 'https://inveservice-fgl-41fc0eb645b0.herokuapp.com/api/v1'
 } 
